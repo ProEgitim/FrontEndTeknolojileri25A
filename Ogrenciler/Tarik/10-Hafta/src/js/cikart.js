2889e166fd86d5ed16267d9f2f46f09aedd11c1f
Tarık Yeşilçimen <tarikyesilcimenn@gmail.com>
2022-03-12 10:55:13 +0300

    hesapla
    hesapla

---
+++ Ogrenciler/Tarik/10-Hafta/src/js/cikart.js
@@ -0,0 +1,5 @@
+function Cikart() {
+    var c1= document.getElementById("c1").value;
+    var c2=document.getElementById("c2").value;
+    document.getElementById("cikartSonuc").innerHTML=parseInt(c1)-parseInt(c2);
+}
