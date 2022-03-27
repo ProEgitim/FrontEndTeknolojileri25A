5ef9f3ab4b9472d748460d9ea7bdc20531c60a3b
Enis Sait ERKEN <saiderken@gmail.com>
2022-03-27 13:57:11 +0300

    Proje ve React Giriş

---
+++ Dersler/12-Hafta/Proje/src/assets/js/movieList.js
@@ -0,0 +1,65 @@
+var arr = [
+    {
+        id: 1,
+        name: 'Batman',
+        description: 'Batman Desc.',
+        rating: 8.4,
+        url: 'details-page-1',
+        image: 'https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX140_CR0,0,140,207_.jpg 140w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX210_CR0,0,210,311_.jpg 210w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX280_CR0,0,280,414_.jpg 280w'
+    },
+    {
+        id: 2,
+        name: 'Bergen',
+        description: 'Batman Desc.',
+        rating: 7.3,
+        url: 'details-page-2',
+        image: 'https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX140_CR0,0,140,207_.jpg 140w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX210_CR0,0,210,311_.jpg 210w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX280_CR0,0,280,414_.jpg 280w'
+    },
+    {
+        id: 3,
+        name: 'Kırmızı',
+        description: 'Batman Desc.',
+        rating: 7.1,
+        url: 'details-page-3',
+        image: 'https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX140_CR0,0,140,207_.jpg 140w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX210_CR0,0,210,311_.jpg 210w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX280_CR0,0,280,414_.jpg 280w'
+    },
+    {
+        id: 4,
+        name: 'Bigfoot Family',
+        description: 'Batman Desc.',
+        rating: 5.8,
+        url: 'details-page-4',
+        image: 'https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX140_CR0,0,140,207_.jpg 140w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX210_CR0,0,210,311_.jpg 210w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX280_CR0,0,280,414_.jpg 280w'
+    },
+    {
+        id: 5,
+        name: 'Kayıp Şehir',
+        description: 'Batman Desc.',
+        rating: 6.8,
+        url: 'details-page-5',
+        image: 'https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX140_CR0,0,140,207_.jpg 140w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX210_CR0,0,210,311_.jpg 210w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX280_CR0,0,280,414_.jpg 280w'
+    },
+    {
+        id: 6,
+        name: 'Kim Demiş Kötüyüz Diye?',
+        description: 'Batman Desc.',
+        rating: 7.1,
+        url: 'details-page-6',
+        image: 'https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX140_CR0,0,140,207_.jpg 140w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX210_CR0,0,210,311_.jpg 210w, https://m.media-amazon.com/images/M/MV5BZjlmYjY0MTEtYjNmOC00OWQ5LWEyMGEtYzYxYTc2MjI4MTVjXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_QL75_UX280_CR0,0,280,414_.jpg 280w'
+    }
+]
+
+$(document).ready(function() {
+    $.each(arr, function(i, item) {
+        $(".movieList").find(".row").append(
+            `<div class="card col-2 bg-dark">
+                <img src="${item.image}" class="card-img-top" alt="${item.description}">
+                <div class="card-body">
+                    <span class="fs-6 text-white">${item.rating}</span>
+                    <p class="fs-5 card-text text-white">${item.name}</p>
+                    <a href="${item.url}" class="btn text-white border d-flex align-items-end justify-content-center">Show Details</a>
+                </div>
+            </div>`
+        )
+    })
+})
