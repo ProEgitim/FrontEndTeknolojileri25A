750f8603921d66e6abdfc729647ce7dd5eef6e1f
BerkCetinel <cetinelberk7@gmail.com>
2022-04-02 10:29:46 +0300

    React

---
+++ Ogrenciler/Berk/13-Hafta/React/first-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src="src/assets/images/logo.png"></img>
+        <a href="index.html">Home Page</a>
+        <a href="movies.html">Movies</a>
+        <a href="watch.html">Watch</a>
+        <a href="contact.html">Contact Page</a>
+    </header>
+    </div>
+  );
+}
+
+export default App;
+//export { App, App2, App3 } çoklu kullanım
---
+++ Ogrenciler/Berk/13-Hafta/React/first-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+//import { App } from './App'
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<App />,document.getElementById('header'));
+//birden çok kullanım
+// ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('menu'));
+// ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('main'));
+// ReactDOM.render(<React.StrictMode><App /></React.StrictMode>,document.getElementById('footer'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
