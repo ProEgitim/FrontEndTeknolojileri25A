02899d2588076a73139bc004c681f0157d8f066a
mesutg0 <mesutg0@hotmail.com>
2022-04-07 13:05:58 +0300

    proptypes

--- Ogrenciler/Mesut/13-Hafta/React/first-app/src/App.js
+++ Ogrenciler/Mesut/13-Hafta/React/first-app/src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
+import PropTypes from 'prop-types'
 import './App.css';
 
-function App() {
+{/* 
+Aşağıdaki const myAppStyle ile arasındaki fark
+<style>
+  .myAppStyle {
+    background-color: 'red',;
+    font-size: 10px;
+    font-weight: 'bold';
+    width: 100%
+  }
+</style> */}
+
+const myAppStyle = {
+  backgroundColor : 'red',
+  fontSize: 10,
+  fontWeight: 'bold',
+  width: '100%'
+}
+
+const App = props => {
+  // console.log(props)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={myAppStyle}>
+      <header>
+        <img src="logo.png" />
+        <a href="index.html">Home Page</a>
+        <a href="movies.html">Movies</a>
+        <a href="watch.html">Watch</a>
+        <a href="contact.html">Contact Page</a>
+        <div> </div>
+        
+    </header>
+
+      <div className='navbar'>
+        <span> Öğrenci Adı: {props.studentName} </span>
+        <span> Araç Model Yılı: {props.carModel} </span>
+      </div>
     </div>
   );
 }
 
+App.propTypes = {
+  studentName: PropTypes.string,
+  carModel: PropTypes.number
+}
+
+App.defaultProps = {
+  studentName: 'Ad',
+  carModel: 2022
+}
+
 export default App;
+//export { App, App2, App3 } çoklu kullanım
