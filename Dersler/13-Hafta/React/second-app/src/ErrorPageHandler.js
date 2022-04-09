2e6571e886b9fd6b046b24d3f3f58c4160157c8b
Enis Sait ERKEN <saiderken@gmail.com>
2022-04-09 08:55:19 +0300

    Update

---
+++ Dersler/13-Hafta/React/second-app/src/ErrorPageHandler.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function ErrorPageHandler() {
+  return (
+    <div className="App">
+      {/* Sayfa Bulunamadı !! */}
+      Sayfa Yükleniyor
+    </div>
+  );
+}
+
+export default ErrorPageHandler
